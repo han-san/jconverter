be304932f67d796c8cd206fb3c5a6dddf3e6ef40
Johan Sandred <johan.sandred@protonmail.com>
2020-04-08 20:20:58 +0200

    Simplify convert function call

--- logic.cpp
+++ logic.cpp
@@ -2,6 +2,7 @@
 
 #include "logic.hpp"
 
+// Distances
 auto static inches_to_meters(double inches) -> double
 {
     return inches / INCHES_PER_METER;
@@ -27,11 +28,22 @@ auto static miles_to_meters(double miles) -> double
     return miles / MILES_PER_METER;
 }
 
-auto static meters_to_miles(double km) -> double
+auto static meters_to_miles(double meters) -> double
 {
-    return km * MILES_PER_METER;
+    return meters * MILES_PER_METER;
 }
 
+auto static meters_to_kilometers(double meters) -> double
+{
+    return meters / 1000.;
+}
+
+auto static kilometers_to_meters(double kilometers) -> double
+{
+    return kilometers * 1000.;
+}
+
+// Temperatures
 auto static kelvin_to_fahrenheit(double kelvin) -> double
 {
     return kelvin * 9. / 5. - 459.67;
@@ -42,6 +54,15 @@ auto static fahrenheit_to_kelvin(double fahrenheit) -> double
     return (fahrenheit + 459.67) * 5.0 / 9.0;
 }
 
+auto static celsius_to_kelvin(double celsius) -> double {
+    return celsius + 273.15;
+}
+
+auto static kelvin_to_celsius(double kelvin) -> double {
+    return kelvin - 273.15;
+}
+
+// Weights
 auto static lbs_to_grams(double lbs) -> double
 {
     return lbs / LBS_PER_GRAM;
@@ -52,24 +73,114 @@ auto static grams_to_lbs(double grams) -> double
     return grams * LBS_PER_GRAM;
 }
 
-auto convert_meter(double value, Convert direction, DistanceUnit unit) -> double
+auto static grams_to_kilograms(double grams) -> double
 {
-    switch (unit) {
-        case DistanceUnit::METERS: {
-            return value;
-        } break;
-        case DistanceUnit::MILES: {
-            return (direction == Convert::FROM) ? miles_to_meters(value) : meters_to_miles(value);
-        } break;
-        case DistanceUnit::FEET: {
-            return (direction == Convert::FROM) ? feet_to_meters(value) : meters_to_feet(value);
-        } break;
-        case DistanceUnit::INCHES: {
-            return (direction == Convert::FROM) ? inches_to_meters(value) : meters_to_inches(value);
-        } break;
-        default: {
-            assert(false);
-            return 1.;
-        } break;
+    return grams / 1000.;
+}
+
+auto static kilograms_to_grams(double kilograms) -> double
+{
+    return kilograms * 1000.;
+}
+
+auto contains(std::unordered_set<Unit> const& unitSet, Unit unit) {
+    return unitSet.find(unit) != unitSet.cend();
+}
+
+// returns empty optional if units are of different types (e.g. distance and temperature)
+auto convert(Unit fromUnit, Unit toUnit, double value) -> std::optional<double>
+{
+    for (auto unitType : unitTypes) {
+        if (contains(*unitType, fromUnit) && !contains(*unitType, toUnit)) {
+            return {};
+        }
     }
-};
+
+    auto const base = [value, fromUnit] () {
+        switch (fromUnit) {
+            // Distances
+            case Unit::METER: {
+                return value;
+            } break;
+            case Unit::KILOMETER: {
+                return kilometers_to_meters(value);
+            } break;
+            case Unit::MILE: {
+                return miles_to_meters(value);
+            } break;
+            case Unit::FOOT: {
+                return feet_to_meters(value);
+            } break;
+            case Unit::INCH: {
+                return inches_to_meters(value);
+            } break;
+
+            // Temperatures
+            case Unit::KELVIN: {
+                return value;
+            } break;
+            case Unit::CELSIUS: {
+                return celsius_to_kelvin(value);
+            } break;
+            case Unit::FAHRENHEIT: {
+                return fahrenheit_to_kelvin(value);
+            } break;
+
+            // Weight
+            case Unit::GRAM: {
+                return value;
+            } break;
+            case Unit::KILOGRAM: {
+                return kilograms_to_grams(value);
+            } break;
+            case Unit::LB: {
+                return lbs_to_grams(value);
+            } break;
+        }
+    }();
+
+    auto const result = [base, toUnit] () {
+        switch (toUnit) {
+            // Distances
+            case Unit::METER: {
+                return base;
+            } break;
+            case Unit::KILOMETER: {
+                return meters_to_kilometers(base);
+            } break;
+            case Unit::MILE: {
+                return meters_to_miles(base);
+            } break;
+            case Unit::FOOT: {
+                return meters_to_feet(base);
+            } break;
+            case Unit::INCH: {
+                return meters_to_inches(base);
+            } break;
+
+            // Temperatures
+            case Unit::KELVIN: {
+                return base;
+            } break;
+            case Unit::CELSIUS: {
+                return kelvin_to_celsius(base);
+            } break;
+            case Unit::FAHRENHEIT: {
+                return kelvin_to_fahrenheit(base);
+            } break;
+
+            // Weight
+            case Unit::GRAM: {
+                return base;
+            } break;
+            case Unit::KILOGRAM: {
+                return grams_to_kilograms(base);
+            } break;
+            case Unit::LB: {
+                return grams_to_lbs(base);
+            } break;
+        }
+    }();
+
+    return result;
+}
--- logic.hpp
+++ logic.hpp
@@ -1,20 +1,88 @@
 #pragma once
 
+#include <unordered_map>
+#include <unordered_set>
+#include <optional>
+
 auto const MILES_PER_METER = 0.0006213711922;
 auto const LBS_PER_GRAM = 0.0022046226218;
 auto const FEET_PER_METER = 3.280839895;
 auto const INCHES_PER_METER = 39.3700787402;
 
-enum class DistanceUnit {
-    METERS,
-    MILES,
-    FEET,
-    INCHES,
+enum class Unit {
+    CELSIUS,
+    FAHRENHEIT,
+    KELVIN,
+
+    KILOMETER,
+    METER,
+    MILE,
+    FOOT,
+    INCH,
+
+    LB,
+    GRAM,
+    KILOGRAM,
 };
 
-enum class Convert {
-    FROM,
-    TO,
+auto const temperatures = std::unordered_set<Unit> {
+    Unit::CELSIUS,
+    Unit::FAHRENHEIT,
+    Unit::KELVIN,
 };
 
-auto convert_meter(double value, Convert direction, DistanceUnit unit) -> double;
+auto const distances = std::unordered_set<Unit> {
+    Unit::KILOMETER,
+    Unit::METER,
+    Unit::MILE,
+    Unit::FOOT,
+    Unit::INCH,
+};
+
+
+auto const weights = std::unordered_set<Unit> {
+    Unit::LB,
+    Unit::KILOGRAM,
+    Unit::GRAM,
+};
+
+auto const unitTypes = {
+    &temperatures,
+    &distances,
+    &weights,
+};
+
+auto const stringToUnitMap = std::unordered_map<std::string_view, Unit> {
+    { "celsius", Unit::CELSIUS },
+    { "c", Unit::CELSIUS },
+    { "fahrenheit", Unit::FAHRENHEIT },
+    { "f", Unit::FAHRENHEIT },
+    { "kelvin", Unit::KELVIN },
+    { "k", Unit::KELVIN },
+
+    { "meter", Unit::METER },
+    { "meters", Unit::METER },
+    { "m", Unit::METER },
+    { "km", Unit::KILOMETER },
+    { "kilometer", Unit::KILOMETER },
+    { "kilometers", Unit::KILOMETER },
+    { "mile", Unit::MILE },
+    { "miles", Unit::MILE },
+    { "foot", Unit::FOOT },
+    { "feet", Unit::FOOT },
+    { "inch", Unit::INCH },
+    { "inches", Unit::INCH },
+
+    { "pound", Unit::LB },
+    { "pounds", Unit::LB },
+    { "lbs", Unit::LB },
+    { "lb", Unit::LB },
+    { "gram", Unit::GRAM },
+    { "g", Unit::GRAM },
+    { "grams", Unit::GRAM },
+    { "kg", Unit::KILOGRAM },
+    { "kilogram", Unit::KILOGRAM },
+    { "kilograms", Unit::KILOGRAM },
+};
+
+auto convert(Unit fromUnit, Unit toUnit, double value) -> std::optional<double>;
